f992c76b6ad8deadd7b82697968865b0dc27fb20
Alexandra Mills <alexansuzanne@gmail.com>
2022-11-11 10:22:17 -0800

    Solved puzzle first try all by myself

---
+++ diff21.py
@@ -0,0 +1,20 @@
+"""Psuedocode:
+    result = 21 - n
+        if result < 0:
+            return abs(result) * 2
+        else:
+            return abs(result)
+"""
+
+from unit_test import unit_test
+
+def diff21(n):
+    result = 21 - n
+    if result <= 0:
+        return abs(result) * 2
+    else:
+        return abs(result)
+
+unit_test(2, diff21(19))
+unit_test(11, diff21(10))
+unit_test(0, diff21(21))
